71d929def2804adf64baf6e62011a18ae650860b
RhyanMezaroba <rhyanmezaroba@hotmail.com>
2025-03-13 19:06:05 -0300

    Homerwork Update - Cesar's Cipher and Variables

--- Atividades/Aula01/Aula01/Controllers/TesteController.cs
+++ Atividades/Aula01/Aula01/Controllers/TesteController.cs
@@ -1,20 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc; // Name space (normalmente chamado)
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Aula01.Controllers
 {
     public class Result
     {
-        public string Texto = string.Empty;
+        public string Texto { get; set; } = string.Empty;
     }
-    public class TesteController : Controller // Utilizando     "Ctrl ." abre uma opção de importação para o Controller 
+
+    public class TesteController : Controller
     {
-        private readonly ILogger<TesteController> _logger; //SNAPE CASE "_logger"
+        private readonly ILogger<TesteController> _logger;
+
         public TesteController(ILogger<TesteController> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet] // Deixando explicito que o Index vai atender o protocolo do HTTP
+        [HttpGet]
         public IActionResult Index()
         {
             return View("Index", new Result());
@@ -24,9 +27,28 @@ namespace Aula01.Controllers
         public IActionResult Index(string texto)
         {
             Result resultado = new();
-            resultado.Texto = texto.ToUpper();
+            resultado.Texto = CifraDeCesar(texto, 3); // Aplica a Cifra de César com deslocamento de 3
 
             return View("Index", resultado);
+        }
+
+        private string CifraDeCesar(string texto, int deslocamento)
+        {
+            char[] buffer = texto.ToCharArray();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                char letra = buffer[i];
+
+                if (char.IsLetter(letra))
+                {
+                    char limite = char.IsUpper(letra) ? 'A' : 'a';
+                    letra = (char)(limite + (letra - limite + deslocamento) % 26);
+                }
+
+                buffer[i] = letra;
+            }
+
+            return new string(buffer);
         }
     }
 }
