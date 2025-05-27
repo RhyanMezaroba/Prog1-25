3ab4fe0db8f278a46bcc24e03f5ff1128c181239
RhyanMezaroba <rhyanmezaroba@hotmail.com>
2025-05-26 21:38:15 -0300

    Update - AULA 05

---
+++ Atividades/Aula05 - POO/Aula 05/Repository/CustomerData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace Repository
+{
+    public class CustomerData
+    {
+        public static List<Customer> Customer { get; set; } = []; // STATIC = independente da tela do programa, essa classe vai ter o mesmo comportamento
+        public static List<Product> Products { get; set; } = [];
+        public static List<Order> Orders { get; set; } = [];
+    }
+}
--- Atividades/Aula05 - POO/Aula 05/Repository/CustomerRepository.cs
+++ Atividades/Aula05 - POO/Aula 05/Repository/CustomerRepository.cs
@@ -1,20 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Modelo;
+﻿using Modelo;
 
 namespace Repository
 {
-    internal class CustomerRepository
+    public class CustomerRepository
     {
-        public Customer Retrieve()
+        public Customer Retrieve(int id)
         {
-            return new Customer();
+            foreach(Customer c in CustomerData.Customer)
+                if (c.Id == id)
+                    return c;
+
+            return null!;
         }
+
+        public List<Customer> RetrieveByName(string name)
+        {
+            List<Customer> ret = new List<Customer>();
+
+            foreach(Customer c in CustomerData.Customer)
+                if(c.Name!.ToLower().Contains(name.ToLower())) // .TOLOWER para fazer a comparação em minúsculo nos dois
+                    ret.Add(c);
+
+            return ret;
+        }
+
+        public List<Customer> RetrieveAll()
+        {
+            return CustomerData.Customer;
+        }
+
         public void Save(Customer customer)
         {
+            customer.Id = GerCount() + 1;
+            CustomerData.Customer.Add(customer);
+        }
+
+        public bool Delete(Customer customer)
+        {
+            return CustomerData.Customer.Remove(customer);
+        }
+
+        public bool DeleteById(int id)
+        {
+            Customer customer = Retrieve(id);
+            
+            if(customer != null)
+               return Delete(customer);
+
+            return false;
+        }
+
+        public void Update(Customer newCustomer)
+        {
+            Customer oldCustomer = Retrieve(newCustomer.Id);
+            oldCustomer.Name = newCustomer.Name;
+            oldCustomer.WorkAddress = newCustomer.WorkAddress;
+            oldCustomer.HomeAddress = newCustomer.HomeAddress;
+        }
+
+        public int GerCount()
+        {
+            return CustomerData.Customer.Count;
         }
     }
 }
