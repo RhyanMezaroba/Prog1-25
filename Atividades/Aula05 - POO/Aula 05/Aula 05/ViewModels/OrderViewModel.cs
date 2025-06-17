1ec605e3821a82acf186f08c262660b66847d76f
RhyanMezaroba <rhyanmezaroba@hotmail.com>
2025-06-16 22:06:19 -0300

    Update - HomeWork

---
+++ Atividades/Aula05 - POO/Aula 05/Aula 05/ViewModels/OrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Modelo;
+
+namespace Aula_05.ViewModels
+{
+    public class OrderViewModel
+    {
+        public List<Customer> Customers { get; set; } = [];
+        public int? CustomerId { get; set; }
+        public List<SelectedItem>? SelectedItems { get; set; }
+    }
+    public class SelectedItem
+    { 
+        public bool IsSelected { get; set; } = false; //aqui o booleano inicia como falso sem ter "false", ou seja, ó fru fru pra não se perder
+        public OrderItem? OrderItem { get; set; } = null!;
+    }
+}
--- Atividades/Aula05 - POO/Aula 05/Repository/OrderRepository.cs
+++ Atividades/Aula05 - POO/Aula 05/Repository/OrderRepository.cs
@@ -1,20 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Modelo;
+﻿using Modelo;
 
 namespace Repository
 {
-    internal class OrderRepository
+    public class OrderRepository
     {
-        public Order Retrieve()
+        public Order Retrieve(int id)
         {
-            return new Order();
+            foreach (Order c in CustomerData.Orders)
+                if (c.Id == id)
+                    return c;
+
+            return null!;
         }
+
+        public List<Order> RetrieveByName(string name)
+        {
+            List<Order> ret = [];
+
+            foreach (Order o in CustomerData.Orders)
+                if (o.Customer!.Name!.ToLower().Contains(name.ToLower())) // .TOLOWER para fazer a comparação em minúsculo nos dois
+                    ret.Add(o);
+
+            return ret;
+        }
+
+        public List<Order> RetrieveAll()
+        {
+            return CustomerData.Orders;
+        }
+
         public void Save(Order order)
         {
+            order.Id = GerCount() + 1;
+            CustomerData.Orders.Add(order);
+        }
+
+        public bool Delete(Order order)
+        {
+            return CustomerData.Orders.Remove(order);
+        }
+
+        public bool DeleteById(int id)
+        {
+            Order order = Retrieve(id);
+
+            if (order != null)
+                return Delete(order);
+
+            return false;
+        }
+
+        public void Update(Order newOrder)
+        {
+            Order oldOrder = Retrieve(newOrder.Id);
+            oldOrder.Customer = newOrder.Customer;
+            oldOrder.OrderDate = newOrder.OrderDate;
+            oldOrder.ShippingAddress = newOrder.ShippingAddress;
+            oldOrder.OrderItems = newOrder.OrderItems;
+        }
+
+        public int GerCount()
+        {
+            return CustomerData.Orders.Count;
         }
     }
 }
