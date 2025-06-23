121a0e11aad915948a10323630097f972a0ec739
RhyanMezaroba <rhyanmezaroba@hotmail.com>
2025-06-23 19:22:50 -0300

    Commit - Update 23/06/25

---
+++ Atividades/Aula05 - POO/Aula 05/Aula 05/ViewModels/OrderItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aula05.ViewModels
+{
+    public class OrderItemViewModel
+    {
+        public int ProductId { get; set; }
+        public required string ProductName { get; set; }
+
+        [Required(ErrorMessage = "O preço é obrigatório meu caro.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser maior que zero seu maluco.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "A quantidade é obrigatória, sem nada é complicado.")]
+        [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser maior que zero monte kkkkk.")]
+        public int Quantity { get; set; }
+
+        public bool IsSelected { get; set; }
+
+        public decimal TotalItemValue => Price * Quantity;
+    }
+}
